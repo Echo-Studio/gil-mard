96eb1df534f0a593ddbdb2b8a05fa05ce5e4437b
Echo-Studio <mahyar.toshehjoo@gmail.com>
2015-06-04 15:45:38 -0700

    initial commit

---
+++ Touch/Assets/Scripts/CreatingBullets.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class CreatingBullets : MonoBehaviour {
+
+	public GameObject MyGameObject;
+	public bool Created = false;
+	public float Power;
+	public Transform MyTransformGameObject;
+	void Update()
+	{
+		if(Input.touchCount == 1)
+		{
+			if(Created == false)
+			{
+				GameObject obj = Instantiate(MyGameObject, transform.position, transform.rotation) as GameObject;
+				obj.GetComponent<Rigidbody>().AddForce(transform.forward * Power);
+				Created = true;
+				StartCoroutine(WatingVoid());
+			}
+		}
+	}
+
+	IEnumerator WatingVoid()
+	{
+			yield return new WaitForSeconds(1);
+			Created = false;
+	}
+}
---
+++ Touch/Assets/Scripts/EnemyBulletDesytoring.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBulletDesytoring : MonoBehaviour {
+
+	void OnTriggerEnter()
+	{
+		Destroy(gameObject);
+	}
+}
