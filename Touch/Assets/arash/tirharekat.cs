479ce45431c82def34c6b2b99bf246844b2a42da
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2015-06-05 21:43:07 +0430

    little warmup on daggers

--- Touch/Assets/arash/tirharekat.cs
+++ Touch/Assets/arash/tirharekat.cs
@@ -9,6 +9,7 @@ public class tirharekat : MonoBehaviour
 	public bool showsmoke = true;
 	bool intraget = false;
 	bool inground = false;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -26,22 +27,38 @@ public class tirharekat : MonoBehaviour
 			intraget = true;
 			showsmoke = false;
 			GetComponent<Rigidbody2D> ().gravityScale = 1;
-			inground=true;
 		} else {
 		}
 	}
 
+	void OnBecameInvisible ()
+	{
+		Destroy (gameObject);
+	}
+
 	void OnCollisionEnter2D (Collision2D coll)
 	{
-		if ( (coll.gameObject.tag == "Knife") && (!inground) ) {
+		if ((coll.gameObject.tag == "Knife")) {
+			if ((coll.gameObject.GetComponent<tirharekat> ().inground))
+				inground = true;
+		}
+		if ((coll.gameObject.tag == "Knife") && (!inground)) {
 			if (showsmoke)
 				Instantiate (smoke, transform.position, Quaternion.identity);
 			Destroy (gameObject);
-		}else{
-			inground=true;
-			GetComponent<Animator>().enabled=false;
+		} else if (coll.gameObject.tag == "Ground") {
+			inground = true;
+			GetComponent<Animator> ().enabled = false;
 		}
+		if (inground) {
+			GetComponent<Animator> ().enabled = false;
+		}
+		if (coll.gameObject.tag == "Ground") {
+			GetComponent<Rigidbody2D> ().isKinematic = true;
 
+			GetComponent<PolygonCollider2D> ().enabled = false;
+		}
+		Debug.Log (coll.gameObject.tag);
 		
 	}
 }
